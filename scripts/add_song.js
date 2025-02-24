f8f0a602b9ad49c54d14d721f0221362f8eb6b4a
Seth Deno <sdeno3@ivytech.edu>
2025-02-23 20:12:12 -0500

    Updated URLs

--- scripts/add_song.js
+++ scripts/add_song.js
@@ -14,7 +14,7 @@ async function addSong() {
         genre: document.querySelector("#genre").value ? document.querySelector("#genre").value.split(",") : []
     }
 
-    const response = await fetch("http://localhost:3000/api/songs", {
+    const response = await fetch("https://exclusive-temporal-screwdriver.glitch.me/api/songs", {
         method: "POST",
         headers:{
             "Content-Type" : "application/json"
